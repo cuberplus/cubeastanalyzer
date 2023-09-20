b61b12fa03cf47627a78fce9aad516f16b0ac355
mpfeifer1 <mikepf97@gmail.com>
2023-09-19 22:37:17 -0700

    Made more progress parsing CSV

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import moment from 'moment';
 
 function App() {
 
@@ -9,7 +10,7 @@ function App() {
     date: Date
   }
 
-  function csvToArr(stringVal: string, splitter: string): Solve[] {
+  function parseCsv(stringVal: string, splitter: string): Solve[] {
     const [keys, ...rest] = stringVal
       .trim()
       .split("\n")
@@ -24,8 +25,8 @@ function App() {
             obj.time = Number(item.at(index));
             break;
           case "date":
-            console.log("date is ",item.at(index))
-            obj.date = new Date(item.at(index) as string);
+            // TODO: I think this is the wrong time zone, should be UTC
+            obj.date = moment(item.at(index), 'YYYY-MM-DD hh:mm:ss').toDate()
             break;
           default:
             //console.log(key + " is an unused column");
@@ -43,25 +44,8 @@ function App() {
 
         let text = file?.text();
         text?.then( (value: string) => {
-          // right here, value is my csv as a string. This now needs to be parsed into a cool object
-          console.log("CSV text is ", value);
-          console.log("Parsed csv is ", parseCsv(value));
+          console.log("Parsed csv is ", parseCsv(value, ','));
         })
-    }
-
-    const parseCsv = (text: string) => {
-      //console.log("thing is ", x);
-      //var myParser = parse(text, {delimiter: ','}, function(data,err) {
-      //  console.log("d", data);
-      //})
-
-      //console.log("p", myParser);
-
-      //const stream = parse({headers: true})
-      //.on('data', row => console.log("row is ", row));
-      //
-      let data = csvToArr(text, ',');
-      console.log(data);
     }
 
   return (
