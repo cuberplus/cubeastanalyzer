b258c1aa95bc9d5d447b7c15e3784578cb5f4fbb
mpfeifer1 <mikepf97@gmail.com>
2023-09-19 22:17:22 -0700

    CSV parsing kinda works

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  interface Solve {
+    time: number
+  }
+
+  function csvToArr(stringVal: string, splitter: string): Solve[] {
+    const [keys, ...rest] = stringVal
+      .trim()
+      .split("\n")
+      .map((item) => item.split(splitter));
+  
+    const formedArr = rest.map((item) => {
+      const obj: Solve = {time: 0};
+      keys.forEach((key, index) => {
+        // TODO: need this for every column
+        switch(key) {
+          case "time":
+            obj.time = Number(item.at(index));
+            break;
+          default:
+            //console.log(key + " is an unused column");
+        }
+      });
+      return obj;
+    });
+    return formedArr;
+  }
+
     const handleClick = () => {
         let dataset = (document.getElementById("uploaded_data") as HTMLInputElement);
         let files: FileList = dataset.files as FileList;
@@ -17,13 +45,18 @@ function App() {
     }
 
     const parseCsv = (text: string) => {
-      let x = parse;
-      console.log("thing is ", x);
+      //console.log("thing is ", x);
       //var myParser = parse(text, {delimiter: ','}, function(data,err) {
       //  console.log("d", data);
       //})
 
       //console.log("p", myParser);
+
+      //const stream = parse({headers: true})
+      //.on('data', row => console.log("row is ", row));
+      //
+      let data = csvToArr(text, ',');
+      console.log(data);
     }
 
   return (
