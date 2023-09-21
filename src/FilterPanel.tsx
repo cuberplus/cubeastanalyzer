f72babb2e199a558a7103869fd94436148e35898
mpfeifer1 <mikepf97@gmail.com>
2023-09-20 18:41:12 -0700

    Started passing through state

--- src/FilterPanel.tsx
+++ src/FilterPanel.tsx
@@ -4,8 +4,19 @@ import { FilterPanelProps, FilterPanelState } from "./Types";
 export class FilterPanel extends React.Component<FilterPanelProps, FilterPanelState> {
     state: FilterPanelState = {solves: []}
 
+
+    static getDerivedStateFromProps(nextProps: FilterPanelProps) {
+        let newState: FilterPanelState = {
+            solves: nextProps.solves
+        }
+
+        // TODO: apply filtering logic here
+
+        return newState;
+    }
+
     render() {
-        console.log("my props are ", this.props.solves)
+        console.log("my length are ", this.state.solves.length)
         return (
             <div>
                 I'm a filter panel
